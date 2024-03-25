40c725c0d0009afef833a439de6bbd356b65f780
Werka Piekacz <weronika.piekacz@interia.pl>
2024-03-25 19:19:09 +0100

    survay

--- trekking-planer/src/app/components/survey/survey.component.ts
+++ trekking-planer/src/app/components/survey/survey.component.ts
@@ -22,6 +22,17 @@ const surveyJson = {
       elements: [
         {
           type: 'radiogroup',
+          name: 'civilwar',
+          title: 'When was the American Civil War?',
+          choices: ['1796-1803', '1810-1814', '1861-1865', '1939-1945'],
+          correctAnswer: '1861-1865',
+        },
+      ],
+    },
+    {
+      elements: [
+        {
+          type: 'radiogroup',
           name: 'libertyordeath',
           title: 'Whose quote is this: "Give me liberty, or give me death"?',
           choicesOrder: 'random',
