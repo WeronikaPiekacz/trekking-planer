e0d6dc6835cce55a6c3b625381207f721df1e339
adamgwozdz00 <adamgwozdz00@gmail.com>
2024-04-03 18:20:35 +0200

    Handle survey response

--- trekking-planer/src/app/components/planing/planing.component.ts
+++ trekking-planer/src/app/components/planing/planing.component.ts
@@ -1,19 +1,27 @@
-import {Component, Inject, OnInit} from '@angular/core';
+import {Component, Inject} from '@angular/core';
 import {ITrekkingEquipmentService} from "../../business-logic/trekking-equipment.service";
 import {TREKKING_SERVICE_TOKEN} from "../../data-access/ai/ai-trekking-equipment.service";
+import {SurveyResponse} from "../survey/survey.component";
 
 @Component({
   selector: 'app-planing',
   templateUrl: './planing.component.html',
   styleUrl: './planing.component.scss'
 })
-export class PlaningComponent implements OnInit {
+export class PlaningComponent {
+
+
   constructor(@Inject(TREKKING_SERVICE_TOKEN) private readonly service: ITrekkingEquipmentService) {
   }
 
-  async ngOnInit(): Promise<void> {
-    console.log(await this.service.generate("Prepare my equipment for a three-day trekking trip, without an overnight stay in a hut in a mountainous area in the summer."))
+
+  async processResponse($event: SurveyResponse) {
+    const response = await this.service.generate(this.generatePrompt($event));
+    console.log(response)
+
   }
 
-
+  private generatePrompt(response: SurveyResponse): string {
+    return `Prepare my equipment for a ${response.period} trekking trip. I will feed myself by ${response.food}. I will stay in a ${response.accommodation}. My trip will take place in the  ${response.place} in ${response.season}.`
+  }
 }
--- trekking-planer/src/app/components/survey/survey.component.ts
+++ trekking-planer/src/app/components/survey/survey.component.ts
@@ -2,6 +2,22 @@ import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {Model, SurveyModel} from 'survey-core';
 import {ContrastLightPanelless} from "survey-core/themes";
 
+enum Category {
+  PLACE = "place",
+  SEASON = "season",
+  ACCOMMODATION = "accommodation",
+  FOOD = "food",
+  PERIOD = "period"
+}
+
+export type SurveyResponse = {
+  place: string,
+  season: string,
+  accommodation: string,
+  food: string,
+  period: string
+}
+
 const surveyJson = {
   title: 'Chose your trip, we will create equipment for you',
   showProgressBar: 'bottom',
@@ -11,11 +27,9 @@ const surveyJson = {
     {
       elements: [
         {
-          type: 'radiogroup',
-          name: 'Place',
+          type: 'comment',
+          name: Category.PLACE,
           title: 'Where do you want to go on a trekking trip?',
-          choices: ['Mountains', 'Desert', 'Flat terrain', 'Jungle'],
-          correctAnswer: 'Mountains',
         },
       ],
     },
@@ -23,10 +37,9 @@ const surveyJson = {
       elements: [
         {
           type: 'radiogroup',
-          name: 'Season',
+          name: Category.SEASON,
           title: 'What time of year do you go on trip?',
           choices: ['spring', 'summer', 'autumn', 'winter'],
-          correctAnswer: 'spring',
         },
       ],
     },
@@ -34,14 +47,13 @@ const surveyJson = {
       elements: [
         {
           type: 'radiogroup',
-          name: 'Accommodation',
+          name: Category.ACCOMMODATION,
           title: 'Where are you planning to spend the night?',
           choices: [
             'tent',
             'shelter',
             'hotel',
-          ],
-          correctAnswer: 'tent',
+          ]
         },
       ],
     },
@@ -49,14 +61,13 @@ const surveyJson = {
       elements: [
         {
           type: 'radiogroup',
-          name: 'Food',
+          name: Category.FOOD,
           title: 'What will you feed on?',
           choices: [
             'Restaurants',
             'Food rations',
             'I will hunt',
           ],
-          correctAnswer: 'I will hunt',
         },
       ],
     },
@@ -64,13 +75,14 @@ const surveyJson = {
       elements: [
         {
           type: 'radiogroup',
-          name: 'Distance',
+          name: Category.PERIOD,
           title: 'How long is your trip?',
           choices: [
-            'short',
-            'long'
+            'one day',
+            'few days - week',
+            'two weeks - month',
+            'month - year'
           ],
-          correctAnswer: 'short',
         },
       ],
     },
@@ -83,7 +95,7 @@ const surveyJson = {
     <survey [model]="surveyModel"></survey>`,
 })
 export class SurveyComponent {
-  @Output() submitSurvey = new EventEmitter<any>();
+  @Output() submitSurvey = new EventEmitter<SurveyResponse>();
 
   @Input() json: object | undefined;
   result: any;
@@ -95,5 +107,8 @@ export class SurveyComponent {
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
     //Add 'implements OnInit' to the class.
     this.surveyModel.applyTheme(ContrastLightPanelless)
+    this.surveyModel.onComplete.add((sender, options) => {
+      this.submitSurvey.emit(sender.data)
+    })
   }
 }
